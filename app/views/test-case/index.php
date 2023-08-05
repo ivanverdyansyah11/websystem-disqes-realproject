4b66b9f2fc737afb2eeaa3242fd161e07365ac6f
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-08-06 03:01:38 +0800

    Progress: Finish Responsive \Test Case Page

--- app/views/test-case/index.php
+++ app/views/test-case/index.php
@@ -4,7 +4,7 @@
             <h4 class="title">Test Case</h4>
         </div>
     </div>
-    <div class="row">
+    <div class="row mb-4">
         <div class="col-12">
             <div class="filter-bar">
                 <div class="filter-button d-flex justify-content-between align-items-center">
@@ -41,14 +41,192 @@
                             </div>
                         </div>
                         <div class="wrapper d-flex gap-2">
-                            <button class="button-primary d-flex align-items-center">
+                            <button type="button" class="button-primary d-flex align-items-center">
                                 <div class="search-icon"></div>
                                 Search
                             </button>
-                            <div class="reset-button button-transparent d-flex align-items-center">
+                            <button type="button" class="reset-button button-transparent d-flex align-items-center">
                                 <div class="reset-icon"></div>
                                 Reset
-                            </div>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12 col-lg-3 mb-4 mb-lg-0">
+            <div class="table-header d-flex justify-content-between align-items-center">
+                <p class="table-title">Test Suites</p>
+                <div class="wrapper-icon" data-bs-toggle="modal" data-bs-target="#addNewSuite">
+                    <div class="add-suite-icon"></div>
+                </div>
+            </div>
+            <a href="#" class="suite-header d-flex align-items-center">
+                <div class="all-suite-icon"></div>
+                <p>All Test Cases</p>
+            </a>
+            <div class="wrapper-suite">
+                <div class="wrapper-header position-relative">
+                    <a href="#" class="suite-header d-flex align-items-center justify-content-between position-relative">
+                        <div class="wrapper-header d-flex align-items-center">
+                            <div class="suite-icon"></div>
+                            <p>Dashboard</p>
+                        </div>
+                        <div class="arrow-suite p-1 pe-0">
+                            <div class="arrow-icon"></div>
+                        </div>
+                    </a>
+                    <div class="wrapper-action d-flex align-items-center">
+                        <div class="box-rotate position-relative"></div>
+                        <div class="card-action d-flex position-relative">
+                            <button type="button" class="wrapper-icon">
+                                <div class="add-section-icon"></div>
+                            </button>
+                            <button type="button" class="wrapper-icon">
+                                <div class="edit-icon"></div>
+                            </button>
+                            <button type="button" class="wrapper-icon">
+                                <div class="delete-icon"></div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <a href="#" class="suite-menu">
+                    <p>Dashboard Sales</p>
+                </a>
+            </div>
+
+            <div class="wrapper-suite">
+                <div class="wrapper-header position-relative">
+                    <a href="#" class="suite-header d-flex align-items-center justify-content-between">
+                        <div class="wrapper-header d-flex align-items-center">
+                            <div class="suite-icon"></div>
+                            <p>Report</p>
+                        </div>
+                        <div class="arrow-suite p-1 pe-0">
+                            <div class="arrow-icon"></div>
+                        </div>
+                    </a>
+                    <div class="wrapper-action d-flex align-items-center">
+                        <div class="box-rotate position-relative"></div>
+                        <div class="card-action d-flex position-relative">
+                            <button type="button" class="wrapper-icon">
+                                <div class="add-section-icon"></div>
+                            </button>
+                            <button type="button" class="wrapper-icon">
+                                <div class="edit-icon"></div>
+                            </button>
+                            <button type="button" class="wrapper-icon">
+                                <div class="delete-icon"></div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <a href="#" class="suite-menu">
+                    <p>Report Sales</p>
+                </a>
+            </div>
+
+            <div class="wrapper-close"></div>
+        </div>
+
+        <div class="col">
+            <div class="table-header d-flex justify-content-between align-items-center">
+                <p class="table-title">Dashboard</p>
+                <a href="#" class="button-primary d-flex align-items-center">
+                    <div class="add-icon"></div>
+                    New
+                </a>
+            </div>
+            <div class="case-header">
+                <p>Name</p>
+            </div>
+
+            <div class="case-menu position-relative">
+                <a href="#">See a graph of the number of sales transactions per day</a>
+                <div class="wrapper-action d-flex align-items-center">
+                    <div class="box-rotate position-relative"></div>
+                    <div class="card-action d-flex position-relative">
+                        <a href="#" type="button" class="wrapper-icon">
+                            <div class="edit-icon"></div>
+                        </a>
+                        <button type="button" class="wrapper-icon">
+                            <div class="delete-icon"></div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="case-menu position-relative">
+                <a href="#">Filter the report date of the number of transactions per day</a>
+                <div class="wrapper-action d-flex align-items-center">
+                    <div class="box-rotate position-relative"></div>
+                    <div class="card-action d-flex position-relative">
+                        <a href="#" type="button" class="wrapper-icon">
+                            <div class="edit-icon"></div>
+                        </a>
+                        <button type="button" class="wrapper-icon">
+                            <div class="delete-icon"></div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="case-menu position-relative">
+                <a href="#">View monthly sales report graphs</a>
+                <div class="wrapper-action d-flex align-items-center">
+                    <div class="box-rotate position-relative"></div>
+                    <div class="card-action d-flex position-relative">
+                        <a href="#" type="button" class="wrapper-icon">
+                            <div class="edit-icon"></div>
+                        </a>
+                        <button type="button" class="wrapper-icon">
+                            <div class="delete-icon"></div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="wrapper-close"></div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="addNewSuite" tabindex="-1" aria-labelledby="addNewSuiteLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content d-flex flex-column">
+                <div class="content-header d-flex justify-content-between align-items-center">
+                    <h4 class="title">Add New Suite</h4>
+                    <div class="wrapper-icon" data-bs-dismiss="modal">
+                        <div class="exit-icon"></div>
+                    </div>
+                </div>
+                <div class="content-body">
+                    <form action="" style="width: 100%; gap: 24px;" class="d-flex flex-column">
+                        <div class="input-wrapper w-100 position-relative">
+                            <p class="caption-input">Name</p>
+                            <input type="text" class="input position-relative" id="nameInputAdd">
+                        </div>
+                        <div class="input-wrapper w-100 position-relative">
+                            <p class="caption-input">Description</p>
+                            <input type="text" class="input position-relative" id="descriptionInputAdd">
+                        </div>
+                        <div class="wrapper d-flex gap-2">
+                            <button class="button-primary d-flex align-items-center">
+                                <div class="save-icon"></div>
+                                Save
+                            </button>
+                            <button type="button" class="reset-button-add-suite button-transparent d-flex align-items-center">
+                                <div class="reset-icon"></div>
+                                Reset
+                            </button>
+                            <button type="button" class="button-transparent d-flex align-items-center" data-bs-dismiss="modal">
+                                <div class="cancel-icon"></div>
+                                Cancel
+                            </button>
                         </div>
                     </form>
                 </div>
@@ -57,12 +235,16 @@
     </div>
 </div>
 
+
 <script>
     const filterButton = document.querySelector('.filter-button');
     const filterBar = document.querySelector('.filter-bar');
     const keyInput = document.querySelector('#keyInput');
     const nameInput = document.querySelector('#nameInput');
     const resetButton = document.querySelector('.reset-button');
+    const arrowSuite = document.querySelectorAll('.arrow-suite');
+    const wrapperHeader = document.querySelectorAll('.wrapper-header');
+    const caseMenu = document.querySelectorAll('.case-menu');
 
     filterButton.addEventListener('click', function() {
         filterBar.classList.toggle('active');
@@ -72,5 +254,36 @@
         keyInput.value = '';
         nameInput.value = '';
         suiteInput.value = 'Choose Suite Selection';
+    });
+
+    arrowSuite.forEach(element => {
+        element.addEventListener('click', function() {
+            const wrapperSuite = element.parentElement.parentElement.parentElement;
+            const suiteMenu = wrapperSuite.querySelector('.suite-menu');
+            this.classList.toggle('active');
+            suiteMenu.classList.toggle('active');
+        });
+    });
+
+    wrapperHeader.forEach(element => {
+        element.addEventListener('mouseover', function() {
+            const wrapperAction = this.querySelector('.wrapper-action');
+            wrapperAction.classList.add('active');
+        });
+        element.addEventListener('mouseout', function() {
+            const wrapperAction = this.querySelector('.wrapper-action');
+            wrapperAction.classList.remove('active');
+        });
+    });
+
+    caseMenu.forEach(element => {
+        element.addEventListener('mouseover', function() {
+            const wrapperAction = this.querySelector('.wrapper-action');
+            wrapperAction.classList.add('active');
+        });
+        element.addEventListener('mouseout', function() {
+            const wrapperAction = this.querySelector('.wrapper-action');
+            wrapperAction.classList.remove('active');
+        });
     });
 </script>
