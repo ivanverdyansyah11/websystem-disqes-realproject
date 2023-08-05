fdb1e321c084ecb018b3311f1d097de30882a79f
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-08-05 21:07:24 +0800

    Progress: Finish Dashboard Page Responsive

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -1,3 +1,42 @@
-<div class="row">
-    <div class="col-12">123</div>
+<div class="content">
+    <div class="row section-gap">
+        <div class="col-12">
+            <h4 class="title">Dashboard</h4>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card-menu d-flex justify-content-between">
+                <div class="wrapper">
+                    <h6 class="menu-title">Test Case</h6>
+                    <h5 class="menu-value">897</h5>
+                </div>
+                <div class="menu-icon d-flex justify-content-center align-items-center">
+                    <div class="test-case-icon"></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card-menu d-flex justify-content-between">
+                <div class="wrapper">
+                    <h6 class="menu-title">Test Ruins</h6>
+                    <h5 class="menu-value">144</h5>
+                </div>
+                <div class="menu-icon d-flex justify-content-center align-items-center">
+                    <div class="test-ruins-icon"></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card-menu d-flex justify-content-between">
+                <div class="wrapper">
+                    <h6 class="menu-title">Status</h6>
+                    <h5 class="menu-value">20%</h5>
+                </div>
+                <div class="menu-icon d-flex justify-content-center align-items-center">
+                    <div class="status-icon"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
