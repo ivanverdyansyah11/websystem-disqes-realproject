dec2617dceaf9f177e409cfa3120ac89d6171f87
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-08-14 04:38:59 +0800

    Progress: Revision Main Project Page

--- app/views/project/index.php
+++ app/views/project/index.php
@@ -7,15 +7,53 @@
         </div>
     </div>
     <div class="row section-gap">
-        <div class="col-12 d-flex justify-content-md-between align-items-center gap-3 gap-md-0">
+        <div class="col-12">
             <h4 class="title">Project</h4>
-            <button type="button" class="button-primary d-flex align-items-center" data-bs-toggle="modal" data-bs-target="#addNewProject">
-                <div class="add-icon"></div>
-                New
-            </button>
         </div>
     </div>
-    <div class="row">
+    <div class="row section-gap">
+        <div class="col-md-4 col-lg-3 mb-4">
+            <button type="button" class="card-project d-flex justify-content-center align-items-center">
+                <div class="wrapper d-flex flex-column align-items-center">
+                    <div class="plus-icon"></div>
+                    <h5 class="title-add-project">New Project</h5>
+                </div>
+            </button>
+        </div>
+        <div class="col-md-4 col-lg-3 mb-4">
+            <div class="card-project d-flex flex-column justify-content-between">
+                <div class="project-header d-flex align-items-center justify-content-between">
+                    <h6 class="project-title">Retail</h6>
+                    <div class="popup-group position-relative">
+                        <div class="wrapper-icon">
+                            <div class="bar-icon"></div>
+                        </div>
+                        <div class="action-popup">
+                            <button type="button" class="popup-button">Edit</button>
+                            <button type="button" class="popup-button">Delete</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="project-body d-flex justify-content-between align-items-center">
+                    <div class="wrapper d-flex align-items-center gap-4">
+                        <div class="testcase-group">
+                            <div class="testcase-icon"></div>
+                            <p class="badge-count">14</p>
+                        </div>
+                        <div class="testsuite-group">
+                            <div class="testsuite-icon"></div>
+                            <p class="badge-count badge-danger">10</p>
+                        </div>
+                    </div>
+                    <div class="wrapper-icon">
+                        <div class="dashboard-icon"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="row">
         <div class="col-12">
             <div class="table-header d-flex align-items-center">
                 <div class="row align-items-center w-100">
@@ -65,7 +103,7 @@
             <?php endforeach; ?>
 
         </div>
-    </div>
+    </div> -->
 
     <div class="modal fade" id="addNewProject" tabindex="-1" aria-labelledby="addNewProjectLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
@@ -203,6 +241,16 @@
     const nameInputEditProject = document.querySelector('#nameInputEditProject');
     const descriptionInputEditProject = document.querySelector('#descriptionInputEditProject');
     const resetButtonEditProject = document.querySelector('.reset-button-edit-project');
+    const popupGroup = document.querySelectorAll('.popup-group');
+
+    popupGroup.forEach(element => {
+        element.addEventListener('click', function(e) {
+            if (e.target.className == 'bar-icon') {
+                const popup = element.querySelector('.action-popup');
+                popup.classList.toggle('active');
+            }
+        });
+    });
 
     resetButtonAddProject.addEventListener('click', function() {
         nameInputAddProject.value = '';
